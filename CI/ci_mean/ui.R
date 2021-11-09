f3dc238aa2f4a5d7fbc5bf23f818e6266590d80d
khalilhassan <Khalil.Hassan@gmx.de>
2021-11-09 10:03:01 +0100

    change ui to first version of change

--- CI/ci_mean/ui.R
+++ CI/ci_mean/ui.R
@@ -1,25 +1,28 @@
-library("shiny")
-library("shinyBS")
+library("shinydashboard")
 
 
 shinyUI(fluidPage(
     
+    # Application title
     titlePanel("Confidence Interval"),
     
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            bsCollapse(id = "cE", multiple = TRUE, open = "Confidence interval parameter",
-                       bsCollapsePanel("Confidence interval parameter", uiOutput("conflevelUI"), uiOutput("sizeUI"), uiOutput("sigmaUI")),
-                       bsCollapsePanel("Sample drawing", uiOutput("goUI"), uiOutput("resetUI"),uiOutput("speedUI")),
-                       bsCollapsePanel("Data choice", uiOutput("datasetUI"), uiOutput("variableUI"))
-            ),
+            uiOutput("conflevelUI"),
+            uiOutput("sizeUI"),
+            uiOutput("sigmaUI"),
+            uiOutput("goUI"),
+            uiOutput("resetUI"),
+            uiOutput("speedUI"),
+            uiOutput("datasetUI"),
+            uiOutput("variableUI"),
             p(actionLink(
                 inputId = "github",
                 label = "Hier",
                 icon = icon("github"),
                 href = "https://github.com/Kale14/mmstat4/tree/main/inst/examples/stat/confidence_mean",
-                onclick = "window.open('https://github.com/Kale14/mmstat4/tree/main/inst/examples/stat/confidence_mean')"), "geht es zum Code der HU")
+                onclick = "window.open('https://github.com/Kale14/mmstat4/tree/main/inst/examples/stat/confidence_mean')"), "kommt ihr zum Code der HU")
         ),
         
         # Show a plot of the generated distribution
